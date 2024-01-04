810b7352ad7755750f1352f11d8d172e2a03ef9c
MOHD NAZIR ANWER <122814116+naziranwer@users.noreply.github.com>
2024-01-04 22:02:37 +0530

    update

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/product/:productId', (req, res) => {
   //Write a code here for endpoint /product/:productId and also print parameter in json form
-   let id=req.params();
+   let id=req.params.productId;
 
    res.status(200).json({'productId':id});
 });
